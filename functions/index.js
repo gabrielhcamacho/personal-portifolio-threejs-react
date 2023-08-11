d8bd205388a4c3f79399336f5aef2fe14275895e
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2023-08-11 10:53:25 -0300

    test names

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ const ses = new aws.SES({
   region: 'us-east-1',
 });
 
-const ORIGINS = ['https://hamishw.com', 'https://www.hamishw.com'];
+const ORIGINS = ['https://gabrielcamacho.vercel.app/', 'https://gabrielcamacho.vercel.app/'];
 const MAX_EMAIL_LENGTH = 512;
 const MAX_MESSAGE_LENGTH = 4096;
 const EMAIL = 'gabriel.hcamacho@outlook.com';
